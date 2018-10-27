ae944320f1463fd0bdc725746627cda6b31b60e5
Yuya Yabe <yuyabee@gmail.com>
2018-10-27 16:43:30 -0700

    Works with sqlite

--- models/charge.py
+++ models/charge.py
@@ -9,7 +9,7 @@ class Charge(Base):
     id = Column(Integer, primary_key=True)
     tca_code = Column(String, unique=True, nullable=False)
     tca_desc = Column(String, nullable=False)
-    filling_date = Column(String, nullable=False)
+    filing_date = Column(String, nullable=False)
     violation_date = Column(String, nullable=False)
     disposition_date = Column(String, nullable=False)
     case_id = Column(Integer, ForeignKey('cases.id'), nullable=False)
--- scraper.py
+++ scraper.py
@@ -7,8 +7,6 @@ from models.charge import Charge
 
 FNULL = open(os.devnull, 'w')
 list_of_href = []
-cases = []
-charges = []
 
 data = subprocess.Popen(['./list.sh', 'montgomery', 'aa', "1"], stdout=subprocess.PIPE).stdout
 soup = BeautifulSoup(data, 'html.parser')
@@ -19,22 +17,20 @@ session = db.get_db_conn()
 for row in table.findAll("tr")[1:]:
     tds = row.findAll("td")
     case = [tds[x].string for x in [2,3,4,5,6]]
-    for cell in tds:
-        a = cell.find("a")
-        if a != None:
-            case.insert(0, a.string)
-            cases.append(case)
-            case_added = session.add(Case(name=case[0], case_number=case[1], case_name=case[2], status=case[3], filing_date=case[4], status_date=case[5]))
-            for link in a['href'].split('=')[1]:
-                data = subprocess.Popen(['./ext.sh', 'montgomery', link], stdout=subprocess.PIPE, stderr=FNULL).stdout
+    a = tds[1].find("a")
+    case.insert(0, a.string)
+    case = Case(name=case[0], case_number=case[1], case_name=case[2], status=case[3], filing_date=case[4], status_date=case[5])
+    session.add(case)
+    link = a['href'].split('=')[1]
+    data = subprocess.Popen(['./ext.sh', 'montgomery', link], stdout=subprocess.PIPE, stderr=FNULL).stdout
 
-                soup = BeautifulSoup(data, 'html.parser')
-                table = soup.find("table", { "class" : "searchList" })
-                if table is not None:
-                    tbody = table.find("tbody")
-                    if tbody is not None:
-                        tr = table.findAll("tr")[1]
-                        tds = tr.findAll("td")
+    soup = BeautifulSoup(data, 'html.parser')
+    table = soup.find("table", { "class" : "searchList" })
+    tbody = table.find("tbody")
+    tr = table.findAll("tr")[1]
+    tds = tr.findAll("td")
 
-                        charges.append([tds[x].string for x in [2,3,4,6,7]])
-                        session.add(Charge(tca_code=case[0], tca_desc=case[1], filing_date=case[2], violation_date=case[3], disposition_date=case[4], case_id=case_added.id))
+    charge = [tds[x].string for x in [2,3,4,6,7]]
+    print(case)
+    print(charge)
+    session.add(Charge(tca_code=charge[0], tca_desc=charge[1], filing_date=charge[2], violation_date=charge[3], disposition_date=charge[4], case_id=case.id))
