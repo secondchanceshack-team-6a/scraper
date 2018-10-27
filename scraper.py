5620f6532f15eb24f6ec7400c1fbf26cb600b1c5
Yuya Yabe <yuyabee@gmail.com>
2018-10-27 15:55:38 -0700

    Refactor

--- scraper.py
+++ scraper.py
@@ -7,15 +7,14 @@ list_of_href = []
 cases = []
 charges = []
 
-result = subprocess.Popen(['./montgomery-list.sh', 'aa', "1"], stdout=subprocess.PIPE)
-data = result.stdout
+data = subprocess.Popen(['./montgomery-list.sh', 'aa', "1"], stdout=subprocess.PIPE).stdout
 soup = BeautifulSoup(data, 'html.parser')
 table = soup.find("table", { "class" : "searchList" })
 
 for row in table.findAll("tr")[1:]:
-    cells = row.findAll("td")
-    case = [cells[x].string for x in [2,3,4,5,6]]
-    for cell in cells:
+    tds = row.findAll("td")
+    case = [tds[x].string for x in [2,3,4,5,6]]
+    for cell in tds:
         a = cell.find("a")
         if a != None:
             list_of_href.append(a['href'].split('=')[1])
@@ -23,8 +22,7 @@ for row in table.findAll("tr")[1:]:
             cases.append(case)
 
 for link in list_of_href:
-    result = subprocess.Popen(['./montgomery-ext.sh', link], stdout=subprocess.PIPE, stderr=FNULL)
-    data = result.stdout
+    data = subprocess.Popen(['./montgomery-ext.sh', link], stdout=subprocess.PIPE, stderr=FNULL).stdout
 
     soup = BeautifulSoup(data, 'html.parser')
     table = soup.find("table", { "class" : "searchList" })
